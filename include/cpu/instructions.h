6a5390db37d73c4864829a50ce23dc1a2221e079
8086ware <kirillos.khan08@outlook.com>
2025-07-14 14:07:24 -0400

    Add instructions.c and header in cpu folder with different instructions

---
+++ include/cpu/instructions.h
@@ -0,0 +1,26 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+void cmp8(Sys8086* sys, uint8_t val1, uint8_t val2);
+void cmp16(Sys8086* sys, uint16_t val1, uint16_t val2);
+
+void inc8(Sys8086* sys, uint8_t* value);
+void dec8(Sys8086* sys, uint8_t* value);
+void inc16(Sys8086* sys, uint16_t* value);
+void dec16(Sys8086* sys, uint16_t* value);
+
+void jmp(Sys8086* sys, uint16_t segment, uint16_t offset);
+
+void mov8(uint8_t* dest, uint8_t* src);
+void mov16(uint16_t* dest, uint16_t* src);
+
+void mul8(Sys8086* sys, uint8_t value);
+void mul16(Sys8086* sys, uint16_t value);
+
+void push(Sys8086* sys, uint16_t value);
+void pop(Sys8086* sys, uint16_t* value);
+
+void add8(Sys8086* sys, uint8_t* to, uint8_t added);
+void add16(Sys8086* sys, uint16_t* to, uint16_t added);
+
+#endif
---
+++ src/cpu/instructions.c
@@ -0,0 +1,178 @@
+/*This file only include basic instructions
+Really only the ones that you can use to do other instructions
+mov, push, jmp, sub, add, etc*/
+
+#include "system.h"
+#include "memory.h"
+#include "cpu/cpu.h"
+
+void mov8(uint8_t* dest, uint8_t* src)
+{
+	*dest = *src;
+}
+
+void mov16(uint16_t* dest, uint16_t* src)
+{
+	*dest = *src;
+}
+
+void push(Sys8086* sys, uint16_t value)
+{
+	sys->cpu.sp.whole -= 2;
+	write_address16(sys, seg_mem(sys->cpu.ss.whole, sys->cpu.sp.whole), value, 0);
+}
+
+void pop(Sys8086* sys, uint16_t* value)
+{
+	*value = read_address16(sys, seg_mem(sys->cpu.ss.whole, sys->cpu.sp.whole), 0);
+	sys->cpu.sp.whole += 2;
+}
+
+void jmp(Sys8086* sys, uint16_t segment, uint16_t offset)
+{
+	sys->cpu.cs.whole = segment;
+	sys->cpu.ip.whole = offset;
+}
+
+void inc8(Sys8086* sys, uint8_t* value)
+{
+	uint8_t old_val = *value;
+
+	(*value)++;
+
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *value);
+	cpu_modify_flag_zero(&sys->cpu, *value);
+	cpu_modify_flag_parity(&sys->cpu, *value);
+	cpu_modify_flag_sign(&sys->cpu, *value, 0);
+}
+
+void dec8(Sys8086* sys, uint8_t* value)
+{
+	uint8_t old_val = *value;
+
+	(*value)--;
+
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *value);
+	cpu_modify_flag_zero(&sys->cpu, *value);
+	cpu_modify_flag_parity(&sys->cpu, *value);
+	cpu_modify_flag_sign(&sys->cpu, *value, 0);
+}
+
+void inc16(Sys8086* sys, uint16_t* value)
+{
+	uint16_t old_val = *value;
+
+	(*value)++;
+
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *value);
+	cpu_modify_flag_zero(&sys->cpu, *value);
+	cpu_modify_flag_parity(&sys->cpu, *value);
+	cpu_modify_flag_sign(&sys->cpu, *value, 1);
+}
+
+void dec16(Sys8086* sys, uint16_t* value)
+{
+	uint16_t old_val = *value;
+
+	(*value)--;
+
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *value);
+	cpu_modify_flag_zero(&sys->cpu, *value);
+	cpu_modify_flag_parity(&sys->cpu, *value);
+	cpu_modify_flag_sign(&sys->cpu, *value, 1);
+}
+
+void cmp16(Sys8086* sys, uint16_t val1, uint16_t val2)
+{
+	uint16_t temp = val1 - val2;
+
+	cpu_modify_flag_carry(&sys->cpu, val1, temp, 1);
+	cpu_modify_flag_half_carry(&sys->cpu, val1, temp);
+	cpu_modify_flag_zero(&sys->cpu, temp);
+	cpu_modify_flag_parity(&sys->cpu, temp);
+	cpu_modify_flag_sign(&sys->cpu, temp, 1);
+	cpu_modify_flag_overflow(&sys->cpu, val1, val2, temp, 1);
+}
+
+void cmp8(Sys8086* sys, uint8_t val1, uint8_t val2)
+{
+	uint8_t temp = val1 - val2;
+
+	cpu_modify_flag_carry(&sys->cpu, val1, temp, 0);
+	cpu_modify_flag_half_carry(&sys->cpu, val1, temp);
+	cpu_modify_flag_zero(&sys->cpu, temp);
+	cpu_modify_flag_parity(&sys->cpu, temp);
+	cpu_modify_flag_sign(&sys->cpu, temp, 0);
+	cpu_modify_flag_overflow(&sys->cpu, val1, val2, temp, 0);
+}
+
+void mul16(Sys8086* sys, uint16_t value)
+{
+	uint32_t product = value * sys->cpu.ax.whole;
+
+	sys->cpu.ax.whole = product & 0x0000ffff; // Store the lower bytes in ax
+	sys->cpu.dx.whole = (product & 0xffff0000) >> 16; // Store the higher bytes in dx
+
+	// Special flag case, don't use cpu_modify_flag* function
+	// If upper bytes are not 0 set the flags
+	if (sys->cpu.dx.whole != 0)
+	{
+		sys->cpu.flag.whole |= FLAG_OVERFLOW;
+		sys->cpu.flag.whole |= FLAG_CARRY;
+	}
+
+	else
+	{
+		sys->cpu.flag.whole &= ~FLAG_OVERFLOW;
+		sys->cpu.flag.whole &= ~FLAG_CARRY;
+	}
+}
+
+void mul8(Sys8086* sys, uint8_t value)
+{
+	uint16_t product = value * sys->cpu.ax.low;
+
+	sys->cpu.ax.whole = product;
+
+	// Special flag case, don't use cpu_modify_flag* function
+	// If upper bytes are not 0 set the flags
+	if (sys->cpu.ax.whole & 0xff00)
+	{
+		sys->cpu.flag.whole |= FLAG_OVERFLOW;
+		sys->cpu.flag.whole |= FLAG_CARRY;
+	}
+
+	else
+	{
+		sys->cpu.flag.whole &= ~FLAG_OVERFLOW;
+		sys->cpu.flag.whole &= ~FLAG_CARRY;
+	}
+}
+
+void add8(Sys8086* sys, uint8_t* to, uint8_t added)
+{
+	uint8_t old_val = *to;
+
+	(*to) += added;
+
+	cpu_modify_flag_carry(&sys->cpu, old_val, *to, 0);
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *to);
+	cpu_modify_flag_zero(&sys->cpu, *to);
+	cpu_modify_flag_parity(&sys->cpu, *to);
+	cpu_modify_flag_sign(&sys->cpu, *to, 0);
+	cpu_modify_flag_overflow(&sys->cpu, old_val, added, *to, 0);
+}
+
+void add16(Sys8086* sys, uint16_t* to, uint16_t added)
+{
+	uint16_t old_val = *to;
+
+	(*to) += added;
+
+	cpu_modify_flag_carry(&sys->cpu, old_val, *to, 1);
+	cpu_modify_flag_half_carry(&sys->cpu, old_val, *to);
+	cpu_modify_flag_zero(&sys->cpu, *to);
+	cpu_modify_flag_parity(&sys->cpu, *to);
+	cpu_modify_flag_sign(&sys->cpu, *to, 1);
+	cpu_modify_flag_overflow(&sys->cpu, old_val, added, *to, 1);
+}
