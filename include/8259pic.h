2da5298a9b89e329935d33958abd4689f7bb3964
8086ware <kirillos.khan08@outlook.com>
2025-07-19 16:05:12 -0400

    Rename pic ports and add command byte inside pic8259

--- include/8259pic.h
+++ include/8259pic.h
@@ -3,14 +3,15 @@
 
 enum pic8259_port
 {
-	MASTER_COMMAND = 0x0020,
-	MASTER_DATA    = 0x0021,
-	SLAVE_COMMAND  = 0x00A0,
-	SLAVE_DATA     = 0x00A1,
+	PIC_MASTER_COMMAND = 0x0020,
+	PIC_MASTER_DATA    = 0x0021,
+	PIC_SLAVE_COMMAND  = 0x00A0,
+	PIC_SLAVE_DATA     = 0x00A1,
 };
 
 typedef struct pic8259
 {	
+	uint8_t command;
 	// -1 if no interrupt, > -1 contains interrupt number
 	int irq;
 	uint8_t imr; // interrupt mask register, will prevent any irq at x bit set
