d4748d8801cd5a579ed0da71a0e6c0507e59cca7
8086ware <kirillos.khan08@outlook.com>
2025-07-19 16:07:54 -0400

    Shorten write_address16 and read_address16

--- src/mem/readandwrite.c
+++ src/mem/readandwrite.c
@@ -23,7 +23,9 @@ void write_address8(Sys8086* sys, uint32_t address, uint8_t value, _Bool port)
 
 void write_address16(Sys8086* sys, uint32_t address, uint16_t value, _Bool port)
 {
-	uint16_t* valueptr = &sys->memory[address];
+	write_address8(sys, address, value & 0x00ff, port);
+	write_address8(sys, address + 1, value & 0xff00, port);
+}
 
 	if (port) // port address space (devices)
 	{
@@ -66,22 +68,6 @@ uint8_t read_address8(Sys8086* sys, uint32_t address, _Bool port)
 
 uint16_t read_address16(Sys8086* sys, uint32_t address, _Bool port)
 {
-	uint16_t* valueptr = &sys->memory[address];
-
-	if (port) // port address space (devices)
-	{
-
-	}
-
-	else // normal address space (physical ram)
-	{
-		switch (address)
-		{
-		default:
-		{
-			return *valueptr;
-			break;
-		}
-		}
-	}
+	uint16_t value = read_address8(sys, address, port);
+	value |= (uint16_t)read_address8(sys, address + 1, port) << 8;
 }
