db7112649511fa8d858f6f06a0fa7d361ddb9831
8086ware <kirillos.khan08@outlook.com>
2025-08-02 20:58:03 -0400

    Initalize some stuff in system.c

--- src/system.c
+++ src/system.c
@@ -32,38 +32,52 @@ Sys8086* init_sys(FILE* image, FILE* bios_rom)
 		sys->cpu.ip.whole = 0x0;
 	}
 
+	// init mda
+	
 	sys->display.mode_ctrl_reg = 0b00011001;
 	sys->display.status_reg = 0b00001001;
 
-	sys->display.cursor_address = 0;
+	// init crtc
+
+	sys->crtc.cursor_address = 0;
 
 	for (int i = 0; i < MDA_RAM_SIZE; i++)
 	{
 		sys->display.ram[i] = 0;
 	}
 
+	// init memory
 	for (int i = 0; i < MAX_MEMORY_8086; i++)
 	{
 		sys->memory[i] = 0;
 	}
 
 	// init pic
-	sys->pic_master.vector_offset = 0x08;
-	sys->pic_slave.vector_offset = 0x70;
+	sys->pic_master.vector_offset = 0x20;
+	sys->pic_slave.vector_offset = 0x1C0;
 
-	sys->pic_master.irq = -1;
-	sys->pic_slave.irq = -1;
+	sys->pic_master.irr = 0;
+	sys->pic_slave.irr = 0;
 
 	sys->pic_master.imr = 0;
 	sys->pic_slave.imr = 0;
 
+	sys->pic_master.isr = 0;
+	sys->pic_slave.isr = 0;
+
+	// init ps2
+	
+	sys->ps2.status_reg = 0;
+
 	sys->cpu.halted = 0;
+
+	sys->cpu.prev_byte_success = 0;
 
 	sys->cpu.flag.whole = 0;
 
+	// load bios if exist and load bootloader raw if not
 	if(bios_rom != NULL)
 	{
-
 		fseek(bios_rom, 0, SEEK_END);
 		int bios_size = ftell(bios_rom);
 		fseek(bios_rom, 0, SEEK_SET);
