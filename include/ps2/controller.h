40d398da3a36d85354be10cc231a8ea517b285cd
8086ware <kirillos.khan08@outlook.com>
2025-08-02 20:40:50 -0400

    Rename some stuff for port and flags in ps2 controller

--- include/ps2/controller.h
+++ include/ps2/controller.h
@@ -3,20 +3,20 @@
 
 #include <stdint.h>
 
-enum PS2_Ctrl_Status_Reg_Option
+enum PS2_Ctrl_Status_Reg_Flags
 {
-	PS2_OUTPUT_BUFFER = 1,
-	PS2_INPUT_BUFFER = 2,
-	PS2_SYSTEM_FLAG = 4,
-	PS2_COMMAND_DATA = 8,
-	PS2_TIME_OUT_ERROR = 64,
-	PS2_PARITY_ERROR = 128,
+	PS2_STATUS_OUTPUT_BUFFER_FLAG = 1,
+	PS2_STATUS_INPUT_BUFFER_FLAG = 2,
+	PS2_STATUS_SYSTEM_FLAG = 4,
+	PS2_STATUS_COMMAND_DATA_FLAG = 8,
+	PS2_STATUS_TIME_OUT_ERROR_FLAG = 64,
+	PS2_STATUS_PARITY_ERROR_FLAG = 128,
 };
 
 enum PS2_Ctrl_Port
 {
 	PS2_DATA_PORT = 0x60,
-	PS2_STATUS_COMMAND_REG = 0x64, // read = Status, write = Command
+	PS2_STATUS_COMMAND_REG_PORT = 0x64, // read = Status, write = Command
 };
 
 typedef struct PS2_Controller 
