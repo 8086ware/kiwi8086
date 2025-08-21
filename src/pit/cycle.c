38f7e08361927e3eb0f677a9430db1ff5ba79088
8086ware <kirillos.khan08@outlook.com>
2025-08-21 17:45:48 -0400

    PIT: Set last tick after PIT ticks and calculate when there is a tick only

--- src/pit/cycle.c
+++ src/pit/cycle.c
@@ -11,16 +11,16 @@ void pit_cycle(Sys8086* sys)
 {
 	uint64_t now_tick = SDL_GetTicksNS();
 
-	int64_t real_current_count0 = sys->pit.timers[0].current_count;
-	int64_t real_current_count1 = sys->pit.timers[1].current_count;
-	int64_t real_current_count2 = sys->pit.timers[2].current_count;
-
-	uint16_t former_count0 = sys->pit.timers[0].current_count;
-	uint16_t former_count1 = sys->pit.timers[1].current_count;
-	uint16_t former_count2 = sys->pit.timers[2].current_count;
-
 	if(now_tick - sys->pit.last_tick >= PIT_NANOSECONDS_PER_CYCLE)
 	{
+		int64_t real_current_count0 = sys->pit.timers[0].current_count;
+		int64_t real_current_count1 = sys->pit.timers[1].current_count;
+		int64_t real_current_count2 = sys->pit.timers[2].current_count;
+
+		uint16_t former_count0 = sys->pit.timers[0].current_count;
+		uint16_t former_count1 = sys->pit.timers[1].current_count;
+		uint16_t former_count2 = sys->pit.timers[2].current_count;
+
 		sys->pit.timers[0].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
 		sys->pit.timers[1].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
 		sys->pit.timers[2].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
@@ -28,14 +28,14 @@ void pit_cycle(Sys8086* sys)
 		real_current_count0 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
 		real_current_count1 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
 		real_current_count2 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-	}
 
-	if(real_current_count0 <= 0 && former_count0 != 0)
-	{
-		sys->pit.timers[0].current_count = sys->pit.timers[0].reload_value;
-		sys->pic_master.irr |= (1 << PIC_IRQ_PIT);
-	}
+		if (real_current_count0 <= 0 && former_count0 != 0)
+		{
+			sys->pit.timers[0].current_count = sys->pit.timers[0].reload_value;
+			sys->pic_master.irr |= (1 << PIC_IRQ_PIT);
+		}
 
-	sys->pit.last_tick = now_tick;
+		sys->pit.last_tick = now_tick;
+	}
 }
 
