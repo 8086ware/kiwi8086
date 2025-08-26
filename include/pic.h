876f5c4ebbce38b67992eb4d274f3585b32c68ed
8086ware <kirillos.khan08@outlook.com>
2025-08-26 01:20:42 -0400

    PIC: Add OCW and ICW handling in the PIC (some of it)

--- include/pic.h
+++ include/pic.h
@@ -37,10 +37,22 @@ enum Pic_ICW // Tell this to each PIC.
 	// ICW2 is the vector offsets
 	// ICW3 for setting the slave pic irq
 
-	ICW4_8086 = 0x01,
-	ICW4_AUTO = 0x02,
+	PIC_ICW4_8086 = 0x01,
+	PIC_ICW4_AUTO = 0x02,
 };
 
+enum Pic_OCW // Tell this to each PIC... AFTER INITALIZING
+{
+	PIC_OCW2_SERVICING = 0x7,
+	PIC_OCW2_EOI = 0x20,
+	PIC_OCW2_SL = 0x40,
+	PIC_OCW2_R = 0x80,
+
+	PIC_OCW3_READ_REGISTER = 0x3,
+	PIC_OCW3_POLL = 0x4,
+	PIC_OCW3_ENABLE = 0x8,
+	PIC_OCW3_SMM = 0x60,
+};
 typedef struct Pic
 {	
 	uint8_t command;
@@ -51,6 +63,9 @@ typedef struct Pic
 	uint16_t vector_offset; // offset from 0000:0000, or the ivt
 
 	uint8_t icw_step;
+
+	uint8_t icw[4];
+	uint8_t ocw[3];
 } Pic;
 
 
--- src/pic/handle_port.c
+++ src/pic/handle_port.c
@@ -6,45 +6,124 @@ uint8_t handle_pic_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read)
 {
 	Pic* pic = NULL;
 
-	if(port == PIC_MASTER_COMMAND_PORT || port == PIC_MASTER_DATA_PORT)
+	if (port == PIC_MASTER_COMMAND_PORT || port == PIC_MASTER_DATA_PORT)
 	{
 		pic = &sys->pic_master;
 	}
 
-	else if(port == PIC_SLAVE_COMMAND_PORT || port == PIC_SLAVE_DATA_PORT)
+	else if (port == PIC_SLAVE_COMMAND_PORT || port == PIC_SLAVE_DATA_PORT)
 	{
 		pic = &sys->pic_slave;
 	}
 
-    switch(port)
+	switch (port)
 	{
 	case PIC_MASTER_COMMAND_PORT:
 	case PIC_SLAVE_COMMAND_PORT:
 	{
-		if(read)
+		if (read)
 		{
 
 		}
 
 		else
 		{
-			switch(value)
+			if (value & PIC_ICW1_INIT)
 			{
-			
+				pic->icw[0] = value;
+				pic->icw_step = 2;
+				pic->imr = 0;
+			}
+
+			else
+			{
+				if (value & PIC_OCW3_ENABLE) // OCW3 selected
+				{
+					pic->ocw[2] = value;
+				}
+
+				else // OCW2 selected
+				{
+					pic->ocw[1] = value;
+					uint8_t ir_acted_upon = pic->ocw[1] & PIC_OCW2_SERVICING;
+				
+					if (pic->ocw[1] & PIC_OCW2_EOI)
+					{
+						for (int i = 0; i < 8; i++)
+						{
+							if (pic->isr & (1 << i))
+							{
+								pic->isr &= ~(1 << i);
+								break;
+							}
+						}
+					}
+				}
 			}
 		}
-		
+
 		break;
 	}
 
 	case PIC_MASTER_DATA_PORT:
 	case PIC_SLAVE_DATA_PORT:
 	{
-		if(read)
+		if (read)
 		{
-			if(sys->pic_master.command == 0)
+			if (pic->command == 0) // OCW1
 			{
-				return sys->pic_master.imr;
+				return pic->imr;
+			}
+		}
+
+		else
+		{
+			if (pic->command == 0 && pic->icw_step == 0) // OCW1 
+			{
+				pic->imr = value;
+			}
+
+			if (pic->icw_step == 2)
+			{
+				pic->vector_offset = value & 0xF8; // in 8086 mode the 5 most significant data bits are the vector offset
+				pic->icw_step = 0;
+
+				if ((pic->icw[0] & PIC_ICW1_SINGLE) == 0)
+				{
+					pic->icw_step = 3;
+				}
+
+				else if (pic->icw[0] & PIC_ICW1_ICW4)
+				{
+					pic->icw_step = 4;
+				}
+
+				else
+				{
+					pic->icw[3] = 0;
+				}
+			}
+
+			else if (pic->icw_step == 3) // icw3 only read when 2 pics
+			{
+				pic->icw[2] = value;
+
+				if (pic->icw[0] & PIC_ICW1_ICW4)
+				{
+					pic->icw_step = 4;
+				}
+
+				else
+				{
+					pic->icw[3] = 0;
+					pic->icw_step = 0;
+				}
+			}
+
+			else if (pic->icw_step == 4)
+			{
+				pic->icw[3] = value;
+				pic->icw_step = 0;
 			}
 		}
 		break;
