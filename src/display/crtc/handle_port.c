4445655d22132caebcfc9f4d30e768be297151c7
8086ware <kirillos.khan08@outlook.com>
2025-09-01 16:24:27 -0400

    display: Fix some CRTC port handling

--- src/display/crtc/handle_port.c
+++ src/display/crtc/handle_port.c
@@ -22,7 +22,45 @@ uint8_t handle_crtc_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read)
 	}
     case CRTC_DATA_REGISTER_PORT:
     {
-        switch(sys->display.crtc.index)
+        if (read)
+        {
+            switch (sys->display.crtc.index)
+            {
+            case CRTC_DATA_CURSOR_START_SCANLINE:
+            {
+                return sys->display.crtc.cursor_start_scan_line;
+                break;
+            }
+            case CRTC_DATA_CURSOR_END_SCANLINE:
+            {
+                return sys->display.crtc.cursor_end_scan_line;
+                break;
+            }
+            case CRTC_DATA_START_ADDRESS_HIGH:
+            {
+                return (sys->display.crtc.start_address & 0xff00) >> 8;
+                break;
+            }
+            case CRTC_DATA_START_ADDRESS_LOW:
+            {
+                return sys->display.crtc.start_address & 0x00ff;
+                break;
+            }
+            case CRTC_DATA_CURSOR_ADDRESS_HIGH:
+            {
+                return (sys->display.crtc.cursor_address & 0xff00) >> 8;
+                break;
+            }
+            case CRTC_DATA_CURSOR_ADDRESS_LOW:
+            {
+                return sys->display.crtc.cursor_address & 0x00ff;
+                break;
+            }
+            }
+        }
+        else
+        {
+            switch (sys->display.crtc.index)
         {
             case CRTC_DATA_CURSOR_START_SCANLINE:
             {
@@ -36,24 +74,35 @@ uint8_t handle_crtc_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read)
             }
             case CRTC_DATA_START_ADDRESS_HIGH:
             {
-                sys->display.crtc.start_address |= (((uint16_t)value) << 8);
+                uint16_t real_value = value;
+                real_value <<= 8;
+                real_value |= (sys->display.crtc.start_address & 0x00ff);
+                sys->display.crtc.start_address = real_value;
                 break;
             }
             case CRTC_DATA_START_ADDRESS_LOW:
             {
-                sys->display.crtc.start_address |= value;
+                uint16_t real_value = value;
+                real_value |= (sys->display.crtc.start_address & 0xff00);
+                sys->display.crtc.start_address = real_value;
                 break;
             }
             case CRTC_DATA_CURSOR_ADDRESS_HIGH:
             {
-                sys->display.crtc.cursor_address |= (((uint16_t)value) << 8);
+                uint16_t real_value = value;
+                real_value <<= 8;
+                real_value |= (sys->display.crtc.cursor_address & 0x00ff);
+                sys->display.crtc.cursor_address = real_value;
                 break;
             }
             case CRTC_DATA_CURSOR_ADDRESS_LOW:
             {
-                sys->display.crtc.cursor_address |= (uint16_t)value;
+                uint16_t real_value = value;
+                real_value |= (sys->display.crtc.cursor_address & 0xff00);
+                sys->display.crtc.cursor_address = real_value;
                 break;
             }
+        }
         }
         break;
     }
