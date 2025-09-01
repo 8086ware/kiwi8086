23331b9fb6672da74993099e20eefd96a4b4164e
8086ware <kirillos.khan08@outlook.com>
2025-09-01 16:23:24 -0400

    PIT: Add access byte support and operating mode enums

--- include/pit.h
+++ include/pit.h
@@ -12,6 +12,24 @@ enum Pit_Port
 	PIT_MODE_COMMAND_REG_PORT = 0x43,
 };
 
+enum Pit_Operating_Mode
+{
+	PIT_OPERATING_INTERRUPT_ON_TERMINAL_COUNT = 0,
+	PIT_OPERATING_HARDWARE_RETRIGGERABLE_ONESHOT = 1,
+	PIT_OPERATING_RATE_GENERATOR = 2,
+	PIT_OPERATING_SQUARE_WAVE_GENERATOR = 3,
+	PIT_OPERATING_SOFTWARE_TRIGGERED_STROBE = 4,
+	PIT_OPERATING_HARDWARE_TRIGGERED_STROBE = 5,
+};
+
+enum Pit_Access_Mode
+{
+	PIT_ACCESS_LATCH_COUNT = 0,
+	PIT_ACCESS_LOBYTE_ONLY = 1,
+	PIT_ACCESS_HIBYTE_ONLY = 2,
+	PIT_ACCESS_LOBYTE_HIBYTE = 3,
+};
+
 typedef struct Pit_timer
 {
 	_Bool received_reload_low;
@@ -19,6 +37,9 @@ typedef struct Pit_timer
 
 	uint16_t current_count;
 	uint16_t reload_value;
+
+	enum Pit_Operating_Mode operating_mode;
+	enum Pit_Access_Mode access_mode;
 } Pit_timer;
 
 typedef struct Pit
--- src/pit/handle_port.c
+++ src/pit/handle_port.c
@@ -12,35 +12,77 @@ uint8_t handle_pit_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read)
 	{
 		if(read)
 		{
-            if(!sys->pit.timers[port - PIT_CHANNEL_0_PORT].sent_current_low)
+            if(!sys->pit.timers[port - PIT_CHANNEL_0_PORT].sent_current_low) // low
             {
                 sys->pit.timers[port - PIT_CHANNEL_0_PORT].sent_current_low = 1;
+
+                if (sys->pit.timers[port - PIT_CHANNEL_0_PORT].access_mode != PIT_ACCESS_HIBYTE_ONLY)
+                {
                 return sys->pit.timers[port - PIT_CHANNEL_0_PORT].current_count & 0x00ff;
             }
 
             else
             {
+                    return 0;
+                }
+            }
+
+            else // high
+            {
                 sys->pit.timers[port - PIT_CHANNEL_0_PORT].sent_current_low = 0;
-                return (sys->pit.timers[port - PIT_CHANNEL_0_PORT].current_count & 0xff000) >> 8;
+
+                if (sys->pit.timers[port - PIT_CHANNEL_0_PORT].access_mode != PIT_ACCESS_LOBYTE_ONLY) 
+                {
+                    return sys->pit.timers[port - PIT_CHANNEL_0_PORT].current_count >> 8;
+                }
+
+                else
+                {
+                    return 0;
+                }
             }
         }
 
 		else
         {
-            if(!sys->pit.timers[port - PIT_CHANNEL_0_PORT].received_reload_low)
+            if(!sys->pit.timers[port - PIT_CHANNEL_0_PORT].received_reload_low) // low
             {
                 sys->pit.timers[port - PIT_CHANNEL_0_PORT].received_reload_low = 1;
-                sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value = value;
+
+                if (sys->pit.timers[port - PIT_CHANNEL_0_PORT].access_mode != PIT_ACCESS_HIBYTE_ONLY)
+                {
+                    sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value &= ~0x00ff;
+                    sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value |= value;
+                }
             }
 
-            else
+            else // high
             {
                 sys->pit.timers[port - PIT_CHANNEL_0_PORT].received_reload_low = 0;
-                uint16_t upper_byte = value << 8;
-                sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value |= upper_byte;
+
+                if (sys->pit.timers[port - PIT_CHANNEL_0_PORT].access_mode != PIT_ACCESS_LOBYTE_ONLY)
+                {
+                    sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value &= ~0xff00;
+                    sys->pit.timers[port - PIT_CHANNEL_0_PORT].reload_value |= (value << 8);
+                }
             }
 		}
 		
+		break;
+            }
+    case PIT_MODE_COMMAND_REG_PORT:
+    {
+        if (read)
+        {
+
+		}
+		
+        else
+        {
+            int channel = (value & 0xC0) >> 6;
+            sys->pit.timers[channel].access_mode = (value & 0x30) >> 4;
+            sys->pit.timers[channel].operating_mode = (value & 0xE) >> 1;
+        }
 		break;
 	}
 	}
