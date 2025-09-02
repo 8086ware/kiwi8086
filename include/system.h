f92343565e4d551b83ef1194b0376b1beaea57b8
8086ware <kirillos.khan08@outlook.com>
2025-09-02 17:31:02 -0400

    system: Add DMA and PPI port function prototype

--- include/system.h
+++ include/system.h
@@ -8,6 +8,8 @@
 #include "pic.h"
 #include "ps2/controller.h"
 #include "pit.h"
+#include "ppi.h"
+#include "dma.h"
 
 #define MAX_MEMORY_8086 1048576
 // Things to emulate:
@@ -29,12 +31,16 @@ typedef struct Sys8086
 	Pic pic_slave;
 	PS2_Controller ps2;
 	Pit pit;
+	Ppi ppi;
+	DMA dma;
 } Sys8086;
 
 Sys8086* init_sys(FILE* image, FILE* bios_rom);
 
+int cpu_process_opcode(Sys8086* sys, enum CPU_Opcode opcode, Register* data_seg, int cur_inst);
 void cpu_exec(Sys8086* sys);
 uint8_t calc_modrm_byte(Sys8086* sys, Register* data_seg, int instruction_address, void** reg, void** regmem, void* imm, _Bool word, _Bool imm_word, _Bool sreg);
+void pic_check_int(Sys8086* sys);
 void pit_cycle(Sys8086* sys);
 void display_render(Sys8086* sys);
 
@@ -43,5 +49,7 @@ uint8_t handle_pic_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
 uint8_t handle_cga_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
 uint8_t handle_crtc_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
 uint8_t handle_pit_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
+uint8_t handle_ppi_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
+uint8_t handle_dma_port(Sys8086* sys, uint16_t port, uint8_t value, _Bool read);
 
 #endif
