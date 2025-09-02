0ea764b88064f373ce473d9dd2830d0185a7aead
8086ware <kirillos.khan08@outlook.com>
2025-09-02 17:48:02 -0400

    PIT: Shorten pit code using a for loop

--- src/pit/cycle.c
+++ src/pit/cycle.c
@@ -11,31 +11,36 @@ void pit_cycle(Sys8086* sys)
 {
 	uint64_t now_tick = SDL_GetTicksNS();
 
-	if(now_tick - sys->pit.last_tick >= PIT_NANOSECONDS_PER_CYCLE)
+	if (now_tick - sys->pit.last_tick >= PIT_NANOSECONDS_PER_CYCLE)
 	{
-		int64_t real_current_count0 = sys->pit.timers[0].current_count;
-		int64_t real_current_count1 = sys->pit.timers[1].current_count;
-		int64_t real_current_count2 = sys->pit.timers[2].current_count;
-
-		uint16_t former_count0 = sys->pit.timers[0].current_count;
-		uint16_t former_count1 = sys->pit.timers[1].current_count;
-		uint16_t former_count2 = sys->pit.timers[2].current_count;
-
-		sys->pit.timers[0].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-		sys->pit.timers[1].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-		sys->pit.timers[2].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-
-		real_current_count0 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-		real_current_count1 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-		real_current_count2 -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
-
-		if (real_current_count0 <= 0 && former_count0 != 0)
+		for (int i = 0; i < 3; i++)
 		{
-			sys->pit.timers[0].current_count = sys->pit.timers[0].reload_value;
-			sys->pic_master.irr |= (1 << PIC_IRQ_PIT);
-		}
+			int64_t real_current_count = sys->pit.timers[i].current_count;
+			uint16_t former_count = sys->pit.timers[i].current_count;
 
-		sys->pit.last_tick = now_tick;
+			sys->pit.timers[i].current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
+
+			real_current_count -= (now_tick - sys->pit.last_tick) / PIT_NANOSECONDS_PER_CYCLE;
+
+			if (real_current_count <= 0 && former_count != 0)
+			{
+				sys->pit.timers[i].current_count = sys->pit.timers[i].reload_value;
+
+				switch (i)
+				{
+				case 0:
+				{
+					sys->pic_master.irr |= (1 << PIC_IRQ_PIT);
+					break;
+				}
+				case 1:
+				{
+					break;
+				}
+				}
+			}
+
+			sys->pit.last_tick = now_tick;
+		}
 	}
 }
-
