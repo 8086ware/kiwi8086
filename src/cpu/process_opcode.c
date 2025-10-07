6602d7ddf797bc639d9c1cbe4b847066f1f1d9c5
8086ware <kirillos.khan08@outlook.com>
2025-10-07 18:05:00 -0400

    cpu: Simply MOV_AL_MOFFS8 and MOV_AX_MOFFS16 instructions

--- src/cpu/process_opcode.c
+++ src/cpu/process_opcode.c
@@ -1498,16 +1498,14 @@ int cpu_process_opcode(Sys8086* sys, enum CPU_Opcode opcode, Register* data_seg,
 	case MOV_AL_MOFFS8: // A0 dd dd
 	{
 		uint16_t moffs8 = read_address16(sys, cur_inst + 1, 0);
-		uint8_t moffs8_value = read_address8(sys, seg_mem(data_seg->whole, moffs8), 0);
-		mov8(sys, &sys->cpu.ax.low, &moffs8_value);
+		mov8(sys, &sys->cpu.ax.low, sys->memory + seg_mem(data_seg->whole, moffs8));
 		ip_increase = 3;
 		break;
 	}
 	case MOV_AX_MOFFS16: // A1 dd dd
 	{
 		uint16_t moffs16 = read_address16(sys, cur_inst + 1, 0);
-		uint16_t moffs16_value = read_address16(sys, seg_mem(data_seg->whole, moffs16), 0);
-		mov16(sys, &sys->cpu.ax.whole, &moffs16_value);
+		mov16(sys, &sys->cpu.ax.whole, (uint16_t*)(sys->memory + seg_mem(data_seg->whole, moffs16)));
 		ip_increase = 3;
 		break;
 	}
